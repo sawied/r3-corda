0630525f2496551c1accf0fad0444c8357df6aff
zhangxiaowei <zhangxiaowei@chinasofti.com>
2019-05-10 10:36:34 +0800

    first blood

---
+++ contracts/src/main/kotlin/com/github/sawied/corda/iou/schema/IOUSchema.kt
@@ -0,0 +1,38 @@
+package com.github.sawied.corda.iou.schema
+
+import net.corda.core.schemas.MappedSchema
+import net.corda.core.schemas.PersistentState
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+
+/**
+ * IOU family
+ */
+object IOUSchema
+
+
+
+object IOUSchemaV1 : MappedSchema(IOUSchema.javaClass,1,mappedTypes = listOf(IOUSchemaV1.PersistentIOU::class.java)){
+
+
+    @Entity
+    @Table(name="iou_states")
+    class PersistentIOU(
+            @Column(name = "lender")
+            var lenderName: String,
+
+            @Column(name = "borrower")
+            var borrowerName: String,
+
+            @Column(name = "value")
+            var value: Int,
+
+            @Column(name = "linear_id")
+            var linearId: UUID
+    ):PersistentState(){
+        constructor():this("","",0,UUID.randomUUID())
+    }
+
+}
---
+++ contracts/src/main/kotlin/com/github/sawied/corda/iou/state/IOUState.kt
@@ -0,0 +1,34 @@
+package com.github.sawied.corda.iou.state
+
+import com.github.sawied.corda.iou.schema.IOUSchemaV1
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
+import net.corda.core.schemas.MappedSchema
+import net.corda.core.schemas.PersistentState
+import net.corda.core.schemas.QueryableState
+
+data class IOUState(
+        val value: Int,
+        val lender: Party,
+        val borrower: Party,
+        override val linearId: UniqueIdentifier=UniqueIdentifier()
+): LinearState, QueryableState {
+
+    override val participants: List<AbstractParty> get() = listOf(lender, borrower)
+
+    override fun generateMappedObject(schema: MappedSchema): PersistentState {
+        return when (schema) {
+            is IOUSchemaV1 -> IOUSchemaV1.PersistentIOU(
+                    this.lender.name.toString(),
+                    this.borrower.name.toString(),
+                    this.value,
+                    this.linearId.id
+            )
+            else -> throw IllegalArgumentException("Unrecognised schema $schema")
+        }
+    }
+
+    override fun supportedSchemas(): Iterable<MappedSchema> = listOf(IOUSchemaV1)
+}
